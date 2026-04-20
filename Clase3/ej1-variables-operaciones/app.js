70ea38e92f214efcf0aad1009d9a4d609b881ade
natyayestaran <natyayestaran@gmail.com>
2026-04-19 22:15:32 -0300

    Ejercicio 1: Variables y operaciones

---
+++ Clase3/ej1-variables-operaciones/app.js
@@ -0,0 +1,11 @@
+const nombre = "Natali Ayestaran";
+const edad = 21;
+const materia = "Desarrollo de Software";
+
+console.log(`Me llamo ${nombre}, tengo ${edad} años y curso ${materia}`);
+
+let contador = 0;
+contador += 1;
+contador += 1;
+contador += 1;
+console.log(contador);
