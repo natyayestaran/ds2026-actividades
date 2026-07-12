3d6fbae2a859dc9d65f23168a7a63425787fc6d2
natyayestaran <natyayestaran@gmail.com>
2026-07-12 19:33:22 -0300

    Clase 13: configurar Docker y entorno backend

---
+++ Clase13/src/components/BookCard.tsx
@@ -0,0 +1,50 @@
+import { Card, Button } from 'react-bootstrap'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+interface Props {
+  titulo: string
+  autor: string
+  imagen: string
+}
+
+function BookCard({ titulo, autor, imagen }: Props) {
+  const [likes, setLikes] = useState(0)
+
+  return (
+    <Card className="h-100 shadow">
+      <Card.Img
+        variant="top"
+        src={imagen}
+      />
+
+      <Card.Body>
+        <Card.Title>
+          {titulo}
+        </Card.Title>
+
+        <Card.Text>
+          {autor}
+        </Card.Text>
+
+        <Button
+          variant="primary"
+          onClick={() => setLikes(likes + 1)}
+        >
+          💖 Me gusta ({likes})
+        </Button>
+
+        <Button
+          as={Link}
+          to="/libros/1"
+          variant="success"
+          className="ms-2"
+        >
+          Ver más
+        </Button>
+      </Card.Body>
+    </Card>
+  )
+}
+
+export default BookCard
